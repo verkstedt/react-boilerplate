4b4865fe9c67c3c88bb210f662fd3c00b6dd05ab
Georgios Giannoutsos Barkas <contact@nuc.gr>
2018-02-07 16:00:59 +0100

    Add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,20 @@ const conf = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
       }
     ]
   },
